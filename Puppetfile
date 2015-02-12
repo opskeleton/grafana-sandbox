e1ef79e09245380f582563e13847991c11cae8d6
ronen <narkisr@gmail.com>
2015-02-12 02:25:51 +0200

    adding grafana influx and nginx to the mix

--- Puppetfile
+++ Puppetfile
@@ -4,5 +4,14 @@ mod 'puppetlabs/stdlib'
 
 mod 'puppetlabs/apt'
 
-mod 'mstrauss/editfile', 
-   :git => 'git://github.com/mstrauss/puppet-editfile.git'
+mod 'echocat/grafana', 
+  :git => 'git://github.com/echocat/puppet-grafana.git'
+
+mod 'justindowning/influxdb', 
+  :git => 'git://github.com/justindowning/puppet-influxdb.git'
+
+mod 'elasticsearch/elasticsearch',
+  :git => 'git://github.com/elasticsearch/puppet-elasticsearch.git'
+
+mod 'jfryman/nginx',
+  :git => 'git://github.com/jfryman/puppet-nginx.git'
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,5 +1,26 @@
-group{ 'puppet': ensure  => present }
+node default {
 
-node 'grafana.local' {
+  class { 'elasticsearch':
+    manage_repo  => true,
+    repo_version => '1.4'
+  }
 
+  include nginx
+
+  nginx::resource::vhost { 'grafana.local':
+      www_root    => '/opt/grafana',
+      listen_port => '8080'
+  } ->
+
+  class {'grafana':
+    influxdb_host         => 'localhost',
+    influxdb_dbpath       => '/db/metrics',
+    influxdb_user         => 'root',
+    influxdb_pass         => 'root',
+    influxdb_grafana_user => 'root',
+    influxdb_grafana_pass => 'root',
+    elasticsearch_host    => 'localhost',
+  }
+
+  class {'influxdb': }
 }
