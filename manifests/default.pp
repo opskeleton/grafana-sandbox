78fc25e4ae98461fd7492699198bdd13acdb1a51
ronen <narkisr@gmail.com>
2015-02-18 18:30:46 +0200

    using common password

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,6 +2,8 @@ node default {
 
   include nginx
 
+  $pass = 'vagrant'
+
   nginx::resource::vhost { 'grafana.local':
       www_root    => '/opt/grafana',
       listen_port => '8080'
@@ -11,18 +13,21 @@ node default {
     influxdb_host         => $ipaddress_eth1,
     influxdb_dbpath       => '/db/metrics',
     influxdb_user         => 'root',
-    influxdb_pass         => 'root',
+    influxdb_pass         => $pass,
     influxdb_grafana_user => 'root',
-    influxdb_grafana_pass => 'root',
+    influxdb_grafana_pass => $pass,
   }
 
   class {'influxdb': }
 
+  ensure_resource('package','curl',{ensure => present})
+
   class {'influxdb::root':
-    next_pass => 'foobar'
-  }
+    next_pass => $pass
+  } ->
 
   influxdb::database {'metrics':
     ensure => present,
+    pass   => $pass
   }
 }
